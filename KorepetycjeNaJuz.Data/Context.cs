4b1f23f9332d5b90f7359d5b6650aa2eaacd4d16
jan.bujanowski <jan.bujanowski@gmail.com>
2018-10-21 22:10:19 +0200

    git init

---
+++ KorepetycjeNaJuz.Data/Context.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace KorepetycjeNaJuz.Data
+{
+    public class KorepetycjeContext
+    {
+    }
+}
---
+++ KorepetycjeNaJuz/DTO/UserDTO.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KorepetycjeNaJuz.Data.DTO
+{
+    public class UserDTO
+    {
+    }
+}
