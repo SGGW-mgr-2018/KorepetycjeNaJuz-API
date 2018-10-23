bb2da0d480293f8eda614127cda6d031047336bf
Bartosz Sowa <b.sowa9@gmail.com>
2018-10-23 21:36:41 +0200

    Add Home Controller

---
+++ KorepetycjeNaJuz/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace KorepetycjeNaJuz.Controllers
+{
+    [Produces("application/json")]
+    [Route("/")]
+    [ApiExplorerSettings(IgnoreApi = true)]
+    public class HomeController : Controller
+    {
+        private const string SwaggerUri = "~/swagger";
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return new RedirectResult(SwaggerUri);
+        }
+    }
+}
