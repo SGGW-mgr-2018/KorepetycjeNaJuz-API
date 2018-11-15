e877b818bbce10418b2a51a4773eaf0e52bfb903
Roman Siry <roman.siryj@gmail.com>
2018-11-16 00:21:04 +0100

    [38] first name and last name in conversation

--- Core/DTO/Message/ConversationDTO.cs
+++ Core/DTO/Message/ConversationDTO.cs
@@ -1,18 +1,28 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using KorepetycjeNaJuz.Core.Models;
 
 namespace KorepetycjeNaJuz.Core.DTO.Message
 {
     public class ConversationDTO
     {
-        public ConversationDTO(IGrouping<int, Models.Message> conversation)
-        {
-            UserId = conversation.Key;
-            LastMessage = new MessageDTO(conversation.OrderByDescending(m => m.DateOfSending).First());
-        }
-
         [Range(1, int.MaxValue, ErrorMessage = "Pole musi być liczbą całkowitą większą niż 0.")]
-        public int UserId { get; }
-        public MessageDTO LastMessage { get; }
+        public int UserId { get; private set; }
+        public MessageDTO LastMessage { get; private set; }
+        public string FirstName { get; private set; }
+        public string LastName { get; private set; }
+
+        public static ConversationDTO Create(IGrouping<int, Models.Message> conversation, Dictionary<int, User> users)
+        {
+            return new ConversationDTO
+            {
+                UserId = conversation.Key,
+                LastMessage = new MessageDTO(conversation.OrderByDescending(m => m.DateOfSending).First()),
+                FirstName = users[conversation.Key].FirstName,
+                LastName = users[conversation.Key].LastName
+            };
+        }
     }
 }
