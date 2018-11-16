4f3b40976bc14d9020f13ac362f688e4b6a149c5
Filip Matysiak <fil1995@gmail.com>
2018-11-16 10:46:05 +0100

    create coachlessoncontroller

---
+++ Core/DTO/CoachLesson/GetCoachLessonsByFiltersDTO.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace KorepetycjeNaJuz.Core.DTO
+{
+    public class GetCoachLessonsByFiltersDTO
+    {
+        [Required(ErrorMessage = "To pole jest wymagane.")]
+        public double Latitiude { get; set; }
+
+        [Required(ErrorMessage = "To pole jest wymagane.")]
+        public double Longitiude { get; set; }
+
+        [Required(ErrorMessage = "To pole jest wymagane.")]
+        public double Radius { get; set; }
+
+        [Required(ErrorMessage = "To pole jest wymagane.")]
+        public string Subject { get; set; }
+
+        public string Level { get; set; }
+
+        public int CoachId { get; set; }
+
+        public DateTime DateFrom { get; set; }
+
+        public DateTime DateTo { get; set; }
+
+
+        [JsonIgnore]
+        public int StudentId { get; set; }
+    }
+}
---
+++ KorepetycjeNaJuz/Controllers/CoachLessonController.cs
@@ -0,0 +1,47 @@
+﻿using KorepetycjeNaJuz.Core.DTO;
+using KorepetycjeNaJuz.Core.Interfaces;
+using KorepetycjeNaJuz.Core.Helpers;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+using System;
+using NLog;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace KorepetycjeNaJuz.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CoachLessonController : ControllerBase
+    {
+        private readonly ILessonService _lessonService;
+        private readonly ICoachLessonService _coachLessonService;
+        private readonly ICoachLessonRepository _coachLessonRepository;
+        private readonly ILogger _logger;
+
+        public CoachLessonController(
+            ILessonService lessonService, 
+            ICoachLessonService coachLessonService)
+        {
+            this._lessonService = lessonService;
+            this._coachLessonService = coachLessonService;
+            this._logger = LogManager.GetLogger("apiLogger");
+        }
+
+
+        /// <summary>
+        /// metoda wyszukująca lekcje.
+        /// </summary>
+        /// <param name="getCoachLessonsByFiltersDTO">Dane lekcji</param>
+        /// <returns>Wygenerowany token JWT</returns>
+        /// <response code="200">Lista dostępnych lekcji</response>
+        [HttpPost,Route("GetCoachLessonsByFilters"), AllowAnonymous]
+        public IActionResult GetCoachLessonsByFilters([Required] GetCoachLessonsByFiltersDTO getCoachLessonsByFiltersDTO)
+        {
+
+            return new JsonResult("");
+        }
+
+    }
+}
