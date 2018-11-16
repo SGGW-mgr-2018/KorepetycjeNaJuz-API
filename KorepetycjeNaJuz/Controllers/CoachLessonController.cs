d839786eefc3df2b38d3a5328853ea65d18db5b2
Filip Matysiak <fil1995@gmail.com>
2018-11-16 15:30:34 +0100

    Linq

--- KorepetycjeNaJuz/Controllers/CoachLessonController.cs
+++ KorepetycjeNaJuz/Controllers/CoachLessonController.cs
@@ -10,6 +10,7 @@ using System.Net;
 using System.Threading.Tasks;
 using System.Collections.Generic;
 using KorepetycjeNaJuz.Core.Models;
+using System.Linq;
 
 namespace KorepetycjeNaJuz.Controllers
 {
@@ -44,58 +45,51 @@ namespace KorepetycjeNaJuz.Controllers
         [HttpPost,Route("GetCoachLessonsByFilters"), AllowAnonymous]
         public IActionResult GetCoachLessonsByFilters([Required] GetCoachLessonsByFiltersDTO getCoachLessonsByFiltersDTO)
         {
-            double distance(double lat1, double lon1, double lat2, double lon2)
-            {
-                double theta = lon1 - lon2;
-                double dist = Math.Sin(deg2rad(lat1)) * Math.Sin(deg2rad(lat2)) + Math.Cos(deg2rad(lat1)) * Math.Cos(deg2rad(lat2)) * Math.Cos(deg2rad(theta));
-                dist = Math.Acos(dist);
-                dist = rad2deg(dist);
-                dist = dist * 60 * 1.1515;
-                dist = dist * 1.609344; // to kilomiters
-                return (dist);
-            }
-            double deg2rad(double deg)
-            {
-                return (deg * Math.PI / 180.0);
-            }
-            double rad2deg(double rad)
-            {
-                return (rad / Math.PI * 180.0);
-            }
-
-            var query = _coachLessonRepository.ListAll();
+            IEnumerable<CoachLesson> query = _coachLessonRepository.ListAll().Where(
+                x => x.LessonStatus.Name == "WaitingForStudents" ||
+                x.LessonStatus.Name == "Reserved" &&
+                x.DateStart >= getCoachLessonsByFiltersDTO.DateFrom &&
+                x.DateEnd <= getCoachLessonsByFiltersDTO.DateTo);
 
             List<CoachLesson> output = new List<CoachLesson>();
             foreach (CoachLesson coachLesson in query)
             {
-                if (coachLesson.DateStart >= getCoachLessonsByFiltersDTO.DateFrom && coachLesson.DateEnd<= getCoachLessonsByFiltersDTO.DateTo) { 
-
-                    if (coachLesson.LessonStatus.Name == "WaitingForStudents" || coachLesson.LessonStatus.Name == "Reserved")
-                    {  // raczej powinno być po id
-                    
-                        // po odległości
-                        if (distance(coachLesson.Address.Latitude, coachLesson.Address.Longitude,getCoachLessonsByFiltersDTO.Latitiude,getCoachLessonsByFiltersDTO.Longitiude) <= getCoachLessonsByFiltersDTO.Radius)
+                // po odległości
+                if (distance(coachLesson.Address.Latitude, coachLesson.Address.Longitude,getCoachLessonsByFiltersDTO.Latitiude,getCoachLessonsByFiltersDTO.Longitiude) <= getCoachLessonsByFiltersDTO.Radius)
+                {
+                    // subject level 
+                    if (coachLesson.Subject.Name == getCoachLessonsByFiltersDTO.Subject && coachLesson.LessonLevel.LevelName == getCoachLessonsByFiltersDTO.Level)
+                    {
+                        if (getCoachLessonsByFiltersDTO.CoachId == 0)
                         {
-                            // subject level 
-                            if (coachLesson.Subject.Name == getCoachLessonsByFiltersDTO.Subject && coachLesson.LessonLevel.LevelName == getCoachLessonsByFiltersDTO.Level)
-                            {
-                                if (getCoachLessonsByFiltersDTO.CoachId == 0)
-                                {
-                                    output.Add(coachLesson);
-                                }else if (coachLesson.CoachId == getCoachLessonsByFiltersDTO.CoachId)
-                                {
-                                    output.Add(coachLesson);
-                                }
-                            }
-
+                            output.Add(coachLesson);
+                        }else if (coachLesson.CoachId == getCoachLessonsByFiltersDTO.CoachId)
+                        {
+                            output.Add(coachLesson);
                         }
                     }
                 }
-               
             }
 
-
             return output.Count < 1 ? StatusCode(404, "Lessons not found.") : StatusCode(200, output);
+        }
+        double distance(double lat1, double lon1, double lat2, double lon2)
+        {
+            double theta = lon1 - lon2;
+            double dist = Math.Sin(deg2rad(lat1)) * Math.Sin(deg2rad(lat2)) + Math.Cos(deg2rad(lat1)) * Math.Cos(deg2rad(lat2)) * Math.Cos(deg2rad(theta));
+            dist = Math.Acos(dist);
+            dist = rad2deg(dist);
+            dist = dist * 60 * 1.1515;
+            dist = dist * 1.609344; // to kilomiters
+            return (dist);
+        }
+        double deg2rad(double deg)
+        {
+            return (deg * Math.PI / 180.0);
+        }
+        double rad2deg(double rad)
+        {
+            return (rad / Math.PI * 180.0);
         }
 
     }
