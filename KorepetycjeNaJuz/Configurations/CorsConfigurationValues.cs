2be2bd564fc483407f1e0e5bf4e6c885e0013f34
Bartosz Sowa <b.sowa9@gmail.com>
2018-12-04 11:28:01 +0100

    Add CORS

---
+++ KorepetycjeNaJuz/Configurations/CorsConfigurationValues.cs
@@ -0,0 +1,9 @@
+﻿namespace KorepetycjeNaJuz.Configurations
+{
+    public class CorsConfigurationValues
+    {
+        public string AllowedOrigin { get; set; }
+        public string AllowedMethods { get; set; }
+        public string AllowedHeaders { get; set; }
+    }
+}
